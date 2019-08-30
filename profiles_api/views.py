1baa9605f8d03ab905ec2a391945f8dea71e245e
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-30 16:39:48 +0530

    Added test HelloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API View"""
+    
+    def get(self, request, format=None):
+        """Return an Hello message"""
+        return Response({'message': 'hello world!'})
