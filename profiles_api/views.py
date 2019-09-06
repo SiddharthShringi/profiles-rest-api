0f96eeda505cfc8c0b352fffa0a64d4d457afa58
Siddharth Shringi <siddharthshringi@gmail.com>
2019-09-06 12:07:03 +0530

    add viesets routes

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from .serializers import HelloSerializer
 
@@ -33,3 +34,36 @@ class HelloAPIView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'method': 'DELETE', 'status': status.HTTP_200_OK})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSets"""
+    serializer_class = HelloSerializer
+    
+    def list(self, request):
+        return Response({'message': 'hello world!'})
+
+    def create(self, request):
+        """Create new object"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message, 'status': status.HTTP_201_CREATED})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'message': 'GET', 'status': status.HTTP_200_OK})
+
+    def update(self, request, pk=None):
+        return Response({'message': 'PUT', 'status': status.HTTP_200_OK})
+
+    def partial_update(self, request, pk=None):
+        return Response({'message': 'PATCH', 'status': status.HTTP_200_OK})
+
+    def destroy(self, request, pk=None):
+        return Response({'message': 'DELETE', 'status': status.HTTP_200_OK})
+
+
